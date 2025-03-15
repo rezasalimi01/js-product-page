ba3aa391afbd373c17b429340baccad0e03d0545
Reza Salimi <rezasalimiofficiall@gmail.com>
2025-03-15 16:38:21 +0330

    Added changing category function and sort products based on their category

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
+
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+        button.classList.add("selected")
+    } else {
+        button.classList.remove("selected")
+    }
+  });
+};
 
 const searchHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
@@ -13,4 +24,30 @@ const searchHandler = (event) => {
   });
 };
 
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
+
+  changeClass(filter)
+
+  products.forEach((product) => {
+    const category = product.dataset.category;
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+      // if (filter === category) {
+      //     product.style.display = "block"
+      // } else {
+      //     product.style.display = "none"
+      // }
+    }
+  });
+};
+
 searchInput.addEventListener("keyup", searchHandler);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", filterHandler);
+});
